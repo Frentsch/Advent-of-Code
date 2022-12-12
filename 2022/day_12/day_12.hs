b4f0ab8466bd47cf6b00ca62981eec88d263206a
Fabio <fabrentsch@gmail.com>
2022-12-12 11:50:47 +0100

    begin day 12

--- 2022/day_12/day_12.hs
+++ 2022/day_12/day_12.hs
@@ -5,4 +5,15 @@ import Prelude
 main = do
     input <- readFile("input.txt")
     let line = lines input
-    print line
+    let startindex = (0,0)
+    print (bfs line [startindex] [startindex] [] 0)
+
+bfs m visited [] nextq depth = if(depth==1) then (length nextq) else bfs m (nextq++visited) nextq [] (depth+1)
+bfs  m visited (t:ts) nextq depth 
+    |elem t visited || x<0 || y<0 ||x>= (length m) || y>= length (m!!0)= bfs m visited ts nextq depth
+    |(m!!x)!!y== 'E' = depth
+    |otherwise = bfs m (visited) ts (nextq++ (if valid m t (x+1,y) then [(x+1,y)] else []) ++(if valid m t (x-1,y) then [(x-1,y)] else [])++ (if valid m t (x,y+1) then [(x,y+1)] else [])++(if valid m t (x,y-1) then [(x,y-1)] else [])) depth
+    where 
+        (x,y) = t
+
+valid m old new = (ord ((m!!(fst old))!!(snd old))) +1>= ord ((m!!(fst new))!!(snd new))
